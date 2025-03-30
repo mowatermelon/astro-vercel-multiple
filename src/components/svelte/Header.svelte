f69f47357bbaa9edf0b5623548f9b00c38068ee2
zaozao <neefoxmo@gmail.com>
2025-03-30 17:11:28 +0800

    update: home page add banner

--- src/components/svelte/Header.svelte
+++ src/components/svelte/Header.svelte
@@ -3,7 +3,12 @@
   console.log('Header', t('site.title'), t('site.subtitle'));
 </script>
 
-<div class="container mx-auto px-4">
-  <h1 class="text-3xl font-bold text-center">{t('site.title')}</h1>
-  <p class="text-center mt-2">{t('site.subtitle')}-{(getCurrentLang())}</p>
+<div>
+  <div class="relative aspect-[21/10] bg-cover bg-center" style="background-image: url('/banner.jpg')">
+    <div class="absolute inset-0  bg-opacity-40"></div>
+  </div>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-4xl font-bold text-center text-gray-800">{t('site.title')}</h1>
+    <p class="text-center mt-4 text-gray-600 text-xl">{t('site.subtitle')}-{(getCurrentLang())}</p>
+  </div>
 </div>
